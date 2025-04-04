cefb6a62739b9c3c09434011bdbf79da5e1d9f62
200431ANTHONYFER <anthonychicana2004@gmail.com>
2025-04-04 18:22:13 -0500

    Update conexion.java

--- src/crud/senati/conexion.java
+++ src/crud/senati/conexion.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package crud.senati1;
+package crud.senati;
 import java.sql.*;
 
 /**
@@ -11,7 +11,7 @@ import java.sql.*;
  */
 public class conexion {
     public Connection conectar(){
-    String url="jdbc:mysql://localhost:3306/senati?zeroDateTimeBehavior=CONVERT_TO_NULL";
+    String url="jdbc:mysql://localhost:3306/crud-senati-anthony?zeroDateTimeBehavior=CONVERT_TO_NULL";
     String user="root";
     String password="";
     Connection cn = null;
